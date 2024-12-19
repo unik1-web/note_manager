a014d3ee4b7316d6da97675deafd3b8dc7d6d920
unik1-web <75004740+unik1-web@users.noreply.github.com>
2024-12-19 05:56:16 +0500

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "Ivan"
+title = "Заголовок"
+content = "Заметка"
+status = "Статус заметки"
+created_date = "10-11-2024"
+issue_date = "10-12-2024"
+print("Имя пользователя:", username)
+print("Заголовок:", title)
+print("Заметка:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки (дедлайн):", issue_date)
