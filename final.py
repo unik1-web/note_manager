2cf1a2e2ab4ce582c3ed0ccf10b917452fed1763
unik1-web <75004740+unik1-web@users.noreply.github.com>
2024-12-22 07:56:27 +0500

    Update final.py

--- final.py
+++ final.py
@@ -1,1 +1,19 @@
-
+note=["Имя пользователя: ","Содержание заметки: ","Статус: ","Дата создания заметки в формате {10-11-2024}: ", "Дата истечения заметки (дедлайн) в формате {10-11-2024}: ",["Заголовок 1","Заголовок 2","Заголовок 3"]]
+note[0] = input(note[0])
+title1 = input("Заголовок 1: ")
+title2 = input("Заголовок 2: ")
+title3 = input("Заголовок 3: ")
+title = [title1, title2, title3]
+content = input("Заметка: ")
+status = input("Статус заметки: ")
+created_date = input("Дата создания заметки в формате {10-11-2024}: ")
+issue_date = input("Дата истечения заметки (дедлайн) в формате {10-11-2024}: ")
+temp_created_date = created_date[0: 5]
+temp_issue_date = issue_date[0: 5]
+print(note)
+print("Имя пользователя:", username)
+print("Заголовок:", title)
+print("Заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", temp_created_date)
+print("Дата истечения заметки (дедлайн):", temp_issue_date)
