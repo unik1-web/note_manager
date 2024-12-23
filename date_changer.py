0bc126fa78d31cbfc7b1ee7723c87ee6e525c680
unik1-web <75004740+unik1-web@users.noreply.github.com>
2024-12-23 14:11:44 +0500

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -7,9 +7,9 @@ created_date = "10-11-2024"
 issue_date = "10-12-2024"
 temp_issue_date = created_date[0: 5]
 temp_created_date = issue_date[0: 5]
-print("Имя пользователя:", username)
-print("Заголовок:", title)
-print("Заметка:", content)
-print("Статус заметки:", status)
-print("Дата создания заметки:", temp_created_date)
-print("Дата истечения заметки (дедлайн):", temp_issue_date)
+print("Имя пользователя: ", username)
+print("Заголовок: ", title)
+print("Заметка: ", content)
+print("Статус заметки: ", status)
+print("Дата создания заметки: ", temp_created_date)
+print("Дата истечения заметки (дедлайн): ", temp_issue_date)
