4d2cdc692042f70aef812f0ba9c032d7a8a49b1f
unik1-web <75004740+unik1-web@users.noreply.github.com>
2024-12-23 14:10:56 +0500

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -5,9 +5,9 @@ content = "Заметка"
 status = "Статус заметки"
 created_date = "10-11-2024"
 issue_date = "10-12-2024"
-print("Имя пользователя:", username)
-print("Заголовок:", title)
-print("Заметка:", content)
-print("Статус заметки:", status)
-print("Дата создания заметки:", created_date)
-print("Дата истечения заметки (дедлайн):", issue_date)
+print("Имя пользователя: ", username)
+print("Заголовок: ", title)
+print("Заметка: ", content)
+print("Статус заметки: ", status)
+print("Дата создания заметки: ", created_date)
+print("Дата истечения заметки (дедлайн): ", issue_date)
