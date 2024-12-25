2517ea1eac693fbd0c16de550bb9030ea053701d
unik1-web <75004740+unik1-web@users.noreply.github.com>
2024-12-25 06:09:50 +0500

    Update add_list.py
    1. В файле date_changer.py вместо created_date[0: 5] можно написать created_date[:5]

--- add_list.py
+++ add_list.py
@@ -8,8 +8,8 @@ content = input("Заметка: ")
 status = input("Статус заметки: ")
 created_date = input("Дата создания заметки в формате {10-11-2024}: ")
 issue_date = input("Дата истечения заметки (дедлайн) в формате {10-11-2024}: ")
-temp_created_date = created_date[0: 5]
-temp_issue_date = issue_date[0: 5]
+temp_created_date = created_date[: 5]
+temp_issue_date = issue_date[: 5]
 print()
 print("Имя пользователя: ", username)
 print("Заголовки: ", title)
