301d57a3df61a68e32481303edd170b130cd350c
unik1-web <75004740+unik1-web@users.noreply.github.com>
2025-01-18 07:28:37 +0500

    Update display_notes_function.py

--- display_notes_function.py
+++ display_notes_function.py
@@ -1,17 +1,19 @@
 # Этап3_Финальное_Юнин_Константин.
 # Задание 3: Функция отображения заметок
-# import pandas as pd
+
+from datetime import datetime
+from tabulate import tabulate
 
 note_states = [
     {'username': 'Алексей', 'title': 'Список покупок',
     'content': 'Купить продукты на неделю', 'status': 'новая',
-    'created_date': '27-11-2024', 'issue_date': '30-11-2024'},
+    'created_date': '21-11-2024', 'issue_date': '30-11-2024'},
     {'username': 'Мария', 'title': 'План учёбы и работы',
      'content': 'Подготовиться к экзамену', 'status': 'в процессе',
      'created_date': '01-12-2024', 'issue_date': '11-12-2024'},
     {'username': 'Иван', 'title': 'План работы',
      'content': 'План работы', 'status': 'выполнено',
-     'created_date': '20-11-2024', 'issue_date': '26-11-2024'}
+     'created_date': '22-11-2024', 'issue_date': '26-11-2024'}
 ]       # Список словарей заметки
 phrase = {
     0: "Какой вывод заметок предпочитаете? (1Заголовки/2Полные данные) ",
@@ -33,85 +35,80 @@ note_keys = (
 
 def display_notes(notes):
     global data_
-    if len(notes) == 0:  # Проверка на наличие заметок
+    if len(notes) == 0:     # Проверка на наличие заметок
         print("У вас нет сохранённых заметок.")
         return
     data_ = data_entry(phrase[0])
     if data_entry(phrase[1]) == 1:
-        notes = sorting_(notes)  # Сортировка списка словарей по дате создания или дедлайну
-    # if data_ == 2:
-        # outp_ = data_entry(phrase[0])
-        # if data_entry(phrase[0]) == 2:
-        #     output_tab(notes)  # Вывод заметок в виде таблицы
-        #     return
-    output_(notes)  # Вывод заметок построчно
+        notes = sorting_(notes)     # Сортировка списка словарей по дате создания или дедлайну
+    if data_ == 2:
+        if data_entry(phrase[2]) == 2:
+            output_tab(notes)       # Вывод заметок в виде таблицы
+            return
+    output_(notes)          # Вывод заметок построчно
 
 
 def data_entry(stroka_):
     while True:
         if stroka_ == phrase[4]:
             input('\033[39m' +
-                  stroka_)  # Изменение цвета текста на стандартный
-            # print('\033[32m')           # Изменение цвета текста на зелёный
+                  stroka_)          # Изменение цвета текста на стандартный
             return
         else:
             date_ = input(stroka_)
             date_ = check_(date_)
-            if date_ == 0:  # Проверка вводимых данных на ошибку
+            if date_ == 0:          # Проверка вводимых данных на ошибку
                 continue
             else:
-                return date_  # Возврат целого числа
+                return date_        # Возврат целого числа
 
 
-def check_(stroka_):  # Проверка вводимых данных
-    if stroka_ not in ["1", "2"]:  # Ввод отличается от 1 или 2
+def check_(stroka_):        # Проверка вводимых данных
+    if stroka_ not in ["1", "2"]:       # Ввод отличается от 1 или 2
         print("Пожалуйста повторите ввод правильно: 1, либо 2!")
         return 0
     else:
-        return int(stroka_)  # Возврат целого числа
+        return int(stroka_)         # Возврат целого числа
 
 
 def sorting_(notes):
     if data_entry(phrase[3]) == 1:      # Сортировка словаря по дате создания или дедлайну
-        sorted_list = sorted(note_states, key=lambda x: x["created_date"])
+        notes.sort(key=lambda x: datetime.strptime(x['created_date'], '%d-%m-%Y'))
     else:
-        sorted_list = sorted(note_states, key=lambda x: x["issue_date"])
-    return sorted_list  # Возврат отсортированного списка словарей
+        notes.sort(key=lambda x: datetime.strptime(x['issue_date'], '%d-%m-%Y'))
+    return notes        # Возврат отсортированного списка словарей
 
-def output_(notes):  # Вывод заметок в виде столбца
-    print('\033[32m' + 'Список заметок:')  # Изменение цвета текста на зелёный
+
+def output_(notes):         # Вывод заметок в виде столбца
+    print('\033[32m' + 'Список заметок:')       # Изменение цвета текста на зелёный
     print("---------------")
     k = 0
     for l, _dict_ in enumerate(notes):
         if data_ == 2:
             if (l % 5) == 0 and l != 0:
                 data_entry(phrase[4])
-            print('\033[32m' + 'Заметка №', (l+1), ':')  # Вывод номера заметки
+            print('\033[32m' + 'Заметка №', (l+1), ':')         # Вывод номера заметки
             for i, res in enumerate(_dict_.keys()):
                 print(
                       f'{note_end[i]}'
                       f'{_dict_[res]}'
-                )  # Вывод заметок по образцу
+                )       # Вывод заметок по образцу
         else:
-            print('\033[32m' + 'Заметка №', (l + 1), ':')  # Вывод номера заметки
+            print('\033[32m' + 'Заметка №', (l + 1), ':')       # Вывод номера заметки
             print(
-                f'{note_end[1]}'  # Вывод заголовков построчно
+                f'{note_end[1]}'        # Вывод заголовков построчно
                 f'{notes[l][note_keys[1]]}'
             )
         print("---------------------")
 
 
-# def output_tab(notes):
-#     print('Список заметок:')
-#     data = notes.values()
-#     df = pd.DataFrame(data, columns=note_end)  # Формирование табличного списка
-#     print(df)
-#     return
-
-
-
+def output_tab(notes):          # Вывод заметок в виде таблицы
+    print('Список заметок:')
+    print('\033[32m' + tabulate(notes, headers='keys'))
+    print('\033[39m')
+    return
 
 
 # Программа
 display_notes(note_states)
-print('\033[39m')  # Изменение цвета текста на стандартный
+print('\033[39m')       # Изменение цвета текста на стандартный
